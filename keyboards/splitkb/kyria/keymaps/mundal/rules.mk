2839384cf58d6c0cacc553088660b420a1ae2f71
Lars Øyvind Mundal <lars.oyvind.mundal@solidsquare.no>
2020-07-31 13:03:27 +0200

    Add new keymap for mundal

---
+++ keyboards/splitkb/kyria/keymaps/mundal/rules.mk
@@ -0,0 +1,5 @@
+OLED_ENABLE = yes   # Enables the use of OLED displays
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # ENables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+UNICODEMAP_ENABLE = yes
