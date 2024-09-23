0c708659a7cfae33cc9b5c10e2622386cf0b95c5
Lars Øyvind Mundal <lars.oyvind.mundal@dnb.no>
2024-09-23 08:24:24 +0200

    update planck keymap

--- keyboards/planck/keymaps/mundal/keymap.c
+++ keyboards/planck/keymaps/mundal/keymap.c
@@ -55,36 +55,34 @@ const uint32_t PROGMEM unicode_map[] = {
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
-
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
+#define ADJUST MO(_ADJUST)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_planck_grid(
       KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_PIPE,
-      KC_ESCAPE,KC_A,KC_S,KC_D,KC_F,KC_G,KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,
+      LCTL_T(KC_ESCAPE),KC_A,KC_S,KC_D,KC_F,KC_G,KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,
       KC_LSHIFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_MINS,
-      KC_MUTE,KC_LCTRL,KC_LALT,KC_LGUI,LT(LOWER,KC_BSPACE),KC_SPACE,KC_NO,LT(RAISE,KC_ENTER),KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT
+      KC_MUTE,KC_LCTRL,KC_LALT,KC_RGUI,LT(LOWER,KC_BSPACE),KC_SPACE,KC_NO,LT(RAISE,KC_ENTER),KC_LALT,KC_VOLD,KC_VOLU,KC_PLY
     ),
 
   [_LOWER] = LAYOUT_planck_grid(
-      KC_TILD,KC_EXLM,KC_AT,KC_LCBR,KC_RCBR,KC_PIPE,_______,_______,_______,_______,_______,KC_BSLS,
+      KC_TILD,KC_EXLM,KC_AT,KC_LCBR,KC_RCBR,KC_PIPE,KC_HOME,KC_END,KC_PGUP,KC_PGDN,_______,KC_BSLS,
       KC_DELETE,KC_HASH,KC_DLR,KC_LPRN,KC_RPRN,KC_GRV,KC_PLUS,KC_MINS,KC_SLSH,KC_ASTR,KC_PERC,KC_QUOTE,
-      _______,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_SCLN,KC_AMPR,KC_EQL,KC_HOME,KC_END,_______,_______,
-      UNICODE_MODE_FORWARD,_______,_______,_______,_______,_______,KC_NO,_______,_______,_______,_______,_______
+      _______,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_SCLN,KC_AMPR,KC_EQL,_______,_______,KC_PGUP,KC_PGDN,
+      _______,_______,_______,_______,_______,UNICODE_MODE_FORWARD,KC_NO,_______,_______,_______,_______,_______
     ),
 
   [_RAISE] = LAYOUT_planck_grid(
       KC_GRAVE,KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,XP(NO_aa, NO_AA),
       KC_DELETE,_______,KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLU,KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,XP(NO_oe, NO_OE),XP(NO_ae, NO_AE),
-      _______,_______,KC_BTN1,KC_BTN2,KC_MUTE,KC_VOLD,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,KC_PGDOWN,KC_PGUP,
-      UNICODE_MODE_FORWARD,_______,_______,_______,_______,_______,KC_NO,_______,_______,_______,_______,_______
+      _______,_______,KC_BTN1,KC_BTN2,KC_MUTE,KC_VOLD,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,KC_HOME,KC_END,
+      _______,_______,_______,_______,_______,KC_TAB,KC_NO,_______,_______,_______,_______,_______
     ),
 
   [_ADJUST] = LAYOUT_planck_grid(
       KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,
-      KC_DELETE,_______,AU_ON,AU_OFF,AU_TOG,_______,_______,_______,_______,_______,_______,RESET,
-      _______,_______,MU_ON,MU_OFF,MU_TOG,_______,_______,_______,_______,_______,_______,_______,
+      _______,_______,AU_ON,AU_OFF,AU_TOG,_______,_______,_______,_______,_______,_______,RESET,
+      _______,_______,MU_ON,MU_OFF,MU_TOG,_______,RESET,_______,_______,_______,_______,_______,
       _______,_______,_______,_______,_______,_______,KC_NO,_______,_______,_______,_______,_______
     ),
 
